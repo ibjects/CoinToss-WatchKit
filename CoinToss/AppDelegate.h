59c5e503ef8317e1769311226327aa84080e894e
Chaudhary Talha <ChTalha@Chaudhary-Talha-Macbook.local>
2015-01-27 16:21:04 +0500

    Initial Commit

---
+++ CoinToss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoinToss
+//
+//  Created by Chaudhary Talha on 1/27/15.
+//  Copyright (c) 2015 MindGem Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoinToss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoinToss
+//
+//  Created by Chaudhary Talha on 1/27/15.
+//  Copyright (c) 2015 MindGem Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
